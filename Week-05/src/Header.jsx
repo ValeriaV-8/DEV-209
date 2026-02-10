1e608fcef280aa99aa61e17c815b2c0ca790fab1
James <james010698@gmail.com>
2026-02-10 12:27:28 -0800

    Build Header and Login

---
+++ Week-05/src/Header.jsx
@@ -0,0 +1,11 @@
+const header = (
+    <header className="header">
+        <h1>To Do List Login</h1>
+    </header>
+);
+
+function Header() {
+    return header;
+}
+
+export default Header;
---
+++ Week-05/src/Login.jsx
@@ -0,0 +1,36 @@
+const login = (
+    <div className = "login">
+        <h2>Login</h2>
+    </div>
+);
+
+const username = (
+    <form className = "loginForm">
+        <label>Username: </label>
+        <input type="text"  id="loginUsername" placeholder="Enter Username" 
+        name="username" required/>
+    </form>
+) 
+
+const password = (
+    <form className = "loginForm">
+        <label>Password: </label>
+        <input type="password"  id="loginPassword" placeholder="Enter Password"
+        name="password" required/>
+    </form>
+)
+
+function Login() {
+    return login;
+};
+
+function Username() {
+    return username;
+}
+
+function Password() {
+    return password;
+}
+
+export default Login;
+export { Username, Password };
